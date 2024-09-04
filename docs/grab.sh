e0d62f22b7a72aad478f872de96f8537737ffc8b
abilz <opssd3klaling@gmail.com>
2024-09-04 07:15:00 +0700

    113

--- docs/grab.sh
+++ docs/grab.sh
@@ -4,5 +4,5 @@ numb=$(expr $num + 1)
 for i in 1 2 3 4 5
 do
 	[[ $i != 1 ]] && page="/page/$i" || page=""
-	curl "https://www.wattpad.com/959075669-wn-part-1-hazure-waku-no-%E3%80%90joutai-ijou-sukiru%E3%80%91de$page" | grep '<p data-p-id' >> "chapter$numb.html"
+	curl "https://www.wattpad.com/959075669-wn-part-1-hazure-waku-no-%E3%80%90joutai-ijou-sukiru%E3%80%91de$page" | grep -E "<p data-p-id[^ ]+>" >> "chapter$numb.html"
 done
