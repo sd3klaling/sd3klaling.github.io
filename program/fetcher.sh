69277ccc860764fb3ee91a445733809270f0a5d7
abilz <opssd3klaling@gmail.com>
2024-09-06 16:28:43 +0700

    fixed fetcher

--- program/fetcher.sh
+++ program/fetcher.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-url="https://www.wattpad.com/$1-wn-part-1-hazure-waku-no-【joutai-ijou-sukiru】de"
+IFS=$'\n'
+url="https://www.wattpad.com/$1-wn-part-1-hazure-waku-no-%E3%80%90joutai-ijou-sukiru%E3%80%91de"
 page=""
 content=""
 temp=""
@@ -9,8 +10,7 @@ chap=0
 #grep -Po '(\{"id(?=.{13}title)|(?<=\{"id":.{11})title)":"?\K[^,"]+' data.txt
 
 function fetch(){
- temp=$(curl -f $url$page)
- 
+ temp=$(curl -L "$url$page" -H 'Cache-Control: no-cache')
 }
 
 function fetchAll(){
@@ -20,14 +20,22 @@ function fetchAll(){
  echo "total pages : "$totalPages
  if [[ $totalPages -gt 1 ]]
  then
+     temp=""
 	 for ((i=2;i<=$totalPages;i++))
 	 do
 		 page="/page/$i"
 		 fetch
-		 content="$content $(grep -oP '<p data-p-id.+</p>' <<< $temp)"
+		 hasil=$(grep -oP '<p data-p-id.+</p>' <<< $temp)
+         content="${content} ${hasil}"
 	 done
  fi
 }
 fetchAll
 chap="$(grep -Po "(?<=\{\"id\":$1,\")title...\K\d+" <<< $temp | head -1)"
-if [[ $content -ne "" ]]; then $content > "../docs/novel/chapter$chap.html"; fi
+
+if [[ -z $content ]]
+then
+    echo "something seems wrong"
+else
+    echo $content > ../docs/novel/chapter$chap.html
+fi
