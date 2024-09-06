60f13fdf1ec29e59e84f93c546f1035984818a92
abilz <opssd3klaling@gmail.com>
2024-09-06 18:45:49 +0700

    html head

--- program/fetcher.sh
+++ program/fetcher.sh
@@ -5,7 +5,7 @@ IFS=$'\n'
 url="https://www.wattpad.com/$1-wn-part-1-hazure-waku-no-%E3%80%90joutai-ijou-sukiru%E3%80%91de"
 page=""
 content=""
-template='<!DOCTYPE html><html><body style="margin: 0;padding: 0;width: 100vw;font-size: 20px;"><div style="padding: 30px 25px;">'
+template='<!DOCTYPE html><head><meta name="viewport" content="width=device-width, initial-scale=1.0"></head><html><body style="margin: 0;padding: 0;width: 100vw;font-size: 20px;"><div style="padding: 30px 25px;">'
 temp=""
 chap=0
 
