0741eeed9c227093d055431cbcd4c099b969e7e3
abilz <opssd3klaling@gmail.com>
2024-09-11 11:20:53 +0700

    test rewrite url

--- docs/grab.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-num=$( ls | grep -Eo '[0-9]+' | sort -rn | head -n 1 )
-numb=$(expr $num + 1)
-for i in 1 2 3 4 5
-do
-	[[ $i != 1 ]] && page="/page/$i" || page=""
-	curl "https://www.wattpad.com/959075669-wn-part-1-hazure-waku-no-%E3%80%90joutai-ijou-sukiru%E3%80%91de$page" | grep -E "<p data-p-id[^ ]+>" >> "chapter$numb.html"
-done
